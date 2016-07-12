7c9aab74e5354460f79f1d405fad82003182c3df
Srđan M <srki94@hotmail.com>
2016-07-12 18:17:13 +0200

    Init

---
+++ Assets/Scripts/Cross The Road/UI/CanvasManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Game.CrossTheRoad
+{
+
+    public class CanvasManager : MonoBehaviour
+    {
+
+        public Image[] imgHearts;
+        public Text txtDucklingCnt;
+
+        public void ReduceLife(int cnt)
+        {
+            for(var i = cnt; cnt <= 0; cnt--)
+            {
+                imgHearts[i].enabled = false;
+            }
+        }
+
+        public void SetDucklingCount(int cnt)
+        {
+            txtDucklingCnt.text = cnt.ToString();
+        }
+    }
+
+}
